58868b854a2163aa24cefa261a9c52ca660931d6
Yeldar <limton301133@gmail.com>
2020-06-10 14:42:36 +0600

    Intro. to Redux

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,18 +4,26 @@ import Header from "../header";
 import Main from "../main";
 import Footer from "../footer";
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from '../../redux/store';
+
+const store = ConfigureStore();
 
 function App() {
 
 
   return (
-    <div className="App">
-        <Router>
-          <Header />
-          <Main />
-          <Footer />
-        </Router>
-    </div>
+      <Provider store={store}>
+          <Router>
+            <div className="App">
+
+                  <Header />
+                  <Main />
+                  <Footer />
+
+            </div>
+          </Router>
+      </Provider>
   );
 }
 
